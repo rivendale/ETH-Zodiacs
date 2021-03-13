8a0493d89147d83f45232e46515f6d3f1a0377c2
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2021-03-13 11:03:12 +0300

    chore (deployment): configure deployment
    - configure dockerfile
    - configure nginx
    
    [Resolves #5]

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 
 const Config = {
-  BASE_URL: process.env.REACT_APP_API_BASE_URL
+  BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://ethsigns.com/api/v1/signs/"
 };
 export default Config;
